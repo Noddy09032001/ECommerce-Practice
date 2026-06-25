17d5f3d81c4dfef845d1fa4d45c2e5bb76f33632
Niranjan Dhokarikar <noddydhokarikar@Niranjans-MacBook-Air.local>
2026-06-25 10:54:04 +0530

    Constants defined for role and gender data

---
+++ backend/ecommerce/src/main/java/com/code/ecommerce/common/constants/GenderConstants.java
@@ -0,0 +1,8 @@
+package com.code.ecommerce.common.constants;
+
+// defining the gender constants
+public enum GenderConstants {
+    MALE,
+    FEMALE,
+    OTHER
+}
---
+++ backend/ecommerce/src/main/java/com/code/ecommerce/common/constants/RoleConstants.java
@@ -0,0 +1,7 @@
+package com.code.ecommerce.common.constants;
+
+// defining the role constants for the users logging in
+public enum RoleConstants {
+    ADMIN,  // can send broadcast messages and campaigns and other stuff
+    USER // buying placing orders and other stuff
+}
