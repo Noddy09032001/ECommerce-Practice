6c8718fdf471ed0a6e42742db5921c33785a223b
Niranjan Dhokarikar <niranjan.dhokarikar2001@gmail.com>
2026-06-28 20:22:47 +0530

    Code changes to the controller for api routings

---
+++ backend/ecommerce/src/main/java/com/code/ecommerce/controller/OrderController.java
@@ -0,0 +1,29 @@
+package com.code.ecommerce.controller;
+
+import com.code.ecommerce.common.constants.RequestTracker;
+import com.code.ecommerce.dto.requests.orders.OrderRequest;
+import com.code.ecommerce.service.OrderService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    private OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService){
+        this.orderService = orderService;
+    }
+
+    @PostMapping("/create-order")
+    public ResponseEntity<?> createOrder(@Valid @RequestBody OrderRequest request,
+                                         @RequestHeader("orderId") String orderId) throws Exception {
+        RequestTracker.addOrderKeys(orderId);  // checking for the order key
+        orderService.createOrder(request);  // generating the order
+        return null;
+    }
+}
