3e5080845f5b079d17fba944fc05895ee909efba
Niranjan Dhokarikar <niranjan.dhokarikar2001@gmail.com>
2026-06-28 20:04:02 +0530

    Modifications and additions in app constants

---
+++ backend/ecommerce/src/main/java/com/code/ecommerce/common/configs/GenericConfigurations.java
@@ -0,0 +1,31 @@
+package com.code.ecommerce.common.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class GenericConfigurations {
+
+    /**
+     *
+     * @return
+     */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://192.168.1.7:3000", "http://192.168.1.8:3000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}
--- backend/ecommerce/src/main/java/com/code/ecommerce/common/constants/RequestTracker.java
+++ backend/ecommerce/src/main/java/com/code/ecommerce/common/constants/RequestTracker.java
@@ -6,8 +6,15 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class RequestTracker {
     private static final Set<String> requestKeysList = ConcurrentHashMap.newKeySet();  // storing the incoming request keys
+    private static final Set<String> orderKeysList = ConcurrentHashMap.newKeySet();  // storing the incoming order keys
+
     public static void addRequestKey(String key){
         if(!requestKeysList.add(key))
             throw new SameRequestException("Request with the same details has already been sent once");
+    }
+
+    public static void addOrderKeys(String key){
+        if(!orderKeysList.add(key))
+            throw new SameRequestException("Request with the same order keys has been sent once");
     }
 }
