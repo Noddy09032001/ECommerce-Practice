9d969e7495d27d31250784c1127a51aeeadf60cb
Niranjan Dhokarikar <niranjan.dhokarikar2001@gmail.com>
2026-06-29 14:07:28 +0530

    Repository layer for merchant and merchant item mapping

---
+++ backend/ecommerce/src/main/java/com/code/ecommerce/repository/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.code.ecommerce.repository;
+
+import com.code.ecommerce.pojo.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+}
