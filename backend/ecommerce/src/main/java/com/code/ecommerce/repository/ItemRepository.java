72fb89732eadd3b1254fc13b976a597ff5c920b6
Niranjan Dhokarikar <niranjan.dhokarikar2001@gmail.com>
2026-06-29 13:16:56 +0530

    Methods added for item service layers

--- backend/ecommerce/src/main/java/com/code/ecommerce/repository/ItemRepository.java
+++ backend/ecommerce/src/main/java/com/code/ecommerce/repository/ItemRepository.java
@@ -2,8 +2,12 @@ package com.code.ecommerce.repository;
 
 import com.code.ecommerce.pojo.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    @Query("SELECT MAX(i.id) FROM Item i")
+    Long getLatestId();   // getting the latest id for the item
 }
