76986b0ff106b1b95f1d22d0a32f200fc845b544
Niranjan Dhokarikar <niranjan.dhokarikar2001@gmail.com>
2026-06-29 14:31:23 +0530

    Methods added for item creation and merchant item mappings

--- backend/ecommerce/src/main/java/com/code/ecommerce/service/ItemService.java
+++ backend/ecommerce/src/main/java/com/code/ecommerce/service/ItemService.java
@@ -1,11 +1,12 @@
 package com.code.ecommerce.service;
 
 import com.code.ecommerce.dto.requests.ItemRequest;
+import com.code.ecommerce.dto.response.ItemResponse;
 
 public interface ItemService {
 
-    void createItem(ItemRequest request) throws Exception;  // creating a new item
-    void updateItem(ItemRequest request) throws Exception;  // updating an existing item
-    void getItemById(Long id) throws Exception;  // fetching an item by the id
+    ItemResponse createItem(ItemRequest request) throws Exception;  // creating a new item
+    ItemResponse updateItem(ItemRequest request) throws Exception;  // updating an existing item
+    ItemResponse getItemById(Long id) throws Exception;  // fetching an item by the id
     void getAllItems() throws Exception;  // getting the list of all the available items
 }
--- backend/ecommerce/src/main/java/com/code/ecommerce/service/ItemServiceImplementation.java
+++ backend/ecommerce/src/main/java/com/code/ecommerce/service/ItemServiceImplementation.java
@@ -2,15 +2,22 @@ package com.code.ecommerce.service;
 
 import com.code.ecommerce.dto.requests.ItemRequest;
 import com.code.ecommerce.dto.requests.SellerItemRequest;
+import com.code.ecommerce.dto.response.ItemResponse;
+import com.code.ecommerce.dto.response.SellerItemResponse;
+import com.code.ecommerce.exceptions.InvalidSellerException;
 import com.code.ecommerce.pojo.Item;
+import com.code.ecommerce.pojo.Seller;
 import com.code.ecommerce.pojo.SellerItemMapping;
 import com.code.ecommerce.repository.ItemRepository;
+import com.code.ecommerce.repository.SellerItemMappingRepository;
+import com.code.ecommerce.repository.SellerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -19,20 +26,33 @@ public class ItemServiceImplementation implements ItemService{
 
     private static final Logger logger = LoggerFactory.getLogger(ItemServiceImplementation.class);
     private final ItemRepository itemRepository;
+    private final SellerRepository sellerRepository;
+    private final SellerItemMappingRepository sellerItemMappingRepository;
 
     @Autowired
-    public ItemServiceImplementation(ItemRepository itemRepository){
+    public ItemServiceImplementation(ItemRepository itemRepository, SellerRepository sellerRepository,
+                                     SellerItemMappingRepository sellerItemMappingRepository){
         this.itemRepository = itemRepository;
+        this.sellerRepository = sellerRepository;
+        this.sellerItemMappingRepository = sellerItemMappingRepository;
     }
 
 
+    /**
+     * Creates a new item from the specified request.
+     *
+     * @param request the item creation request
+     * @return the created item response
+     * @throws Exception if the item creation process fails
+     */
     @Override
-    public void createItem(ItemRequest request) throws Exception {
+    public ItemResponse createItem(ItemRequest request) throws Exception {
         logger.info("Inside the create item method - ");
         try{
             Item item = new Item();
             item.setItemName(request.getName());  // setting the item name
             item.setItemDescription(request.getDescription());  // setting the description
+            item.setCategory(request.getCategory());  // setting the item category
             item.setCreatedOn(LocalDateTime.now());  // setting the created date
             item.setModifiedOn(LocalDateTime.now());  // setting the modified date
 
@@ -47,7 +67,12 @@ public class ItemServiceImplementation implements ItemService{
             item.setSku(sku);  // setting the sku for the item
 
             Item currentItem = itemRepository.save(item);  // saving and retrieving the current item
-            calculateAmountWithTaxForAllSellersForItem(request.getSellers(), request);
+            List<SellerItemResponse> merchants = this.generateSellerItemMappings(request.getSellers(), request, currentItem);  // getting the merchant data
+
+            ItemResponse response = generateResponse(currentItem);  // generating response
+            response.setMerchants(merchants);  // setting the merchant data
+
+            return response;
 
         } catch (Exception e) {
             logger.info("Error creating item - {}", e.getMessage());
@@ -56,10 +81,10 @@ public class ItemServiceImplementation implements ItemService{
     }
 
     @Override
-    public void updateItem(ItemRequest request) throws Exception {
+    public ItemResponse updateItem(ItemRequest request) throws Exception {
         logger.info("Inside the update item method - ");
         try{
-
+            return null;
         } catch (Exception e) {
             logger.info("Error updating item - {}", e.getMessage());
             throw new RuntimeException(e);
@@ -67,10 +92,11 @@ public class ItemServiceImplementation implements ItemService{
     }
 
     @Override
-    public void getItemById(Long id) throws Exception {
+    public ItemResponse getItemById(Long id) throws Exception {
         logger.info("Inside the fetch item by id method - ");
         try{
 
+            return null;
         } catch (Exception e) {
             logger.info("Error fetching item by id - {}", e.getMessage());
             throw new RuntimeException(e);
@@ -90,6 +116,7 @@ public class ItemServiceImplementation implements ItemService{
 
     /**
      * method to generate the sku id for the given item
+     *
      * @param category the category which the item belongs to
      * @return the generated sku for the item
      */
@@ -105,15 +132,121 @@ public class ItemServiceImplementation implements ItemService{
     }
 
     /**
-     * Calculates the total amount by adding all applicable taxes to the base amount.
-     * @param request the item request containing amount and tax percentages
-     * @return the final amount after tax calculation
+     * Generates seller-item mappings for the specified item.
+     *
+     * @param sellerItemRequest the list of seller item requests
+     * @param itemRequest the item request containing item details
+     * @param item the item entity to be associated with the sellers
+     * @return responses for the mappings generated between merchant and the items
      */
-    public void calculateAmountWithTaxForAllSellersForItem(List<SellerItemRequest> request, ItemRequest item){
-        for (SellerItemRequest itemRequest : request){
+    public List<SellerItemResponse> generateSellerItemMappings(List<SellerItemRequest> sellerItemRequest, ItemRequest itemRequest,
+                                           Item item){
+        List<SellerItemResponse> responses = new ArrayList<>();  // storing the responses
+
+        for (SellerItemRequest sellerItemRequest1 : sellerItemRequest){
             SellerItemMapping mapping = new SellerItemMapping();
-            mapping.set
+            mapping.setItem(item);  // setting the item
+            mapping.setOtherCharges(sellerItemRequest1.getOtherCharges());  // setting other charges for the item
+            mapping.setTransportationCharges(sellerItemRequest1.getTransportationCharges());  // setting the transportation charges
+            mapping.setCreatedOn(LocalDateTime.now());  // setting the created time
+            mapping.setModifiedOn(LocalDateTime.now());  // setting the modified time
+
+            Seller currentSeller = this.getSellerById(sellerItemRequest1.getSellerId());
+            mapping.setSeller(currentSeller);  // setting the seller for the item
+
+            mapping.setAmount(sellerItemRequest1.getAmount());  // setting the initial amount
+            mapping.setActive(true); // setting the mapping to active
+
+            // getting the final amount with the tax calculations
+            Double totalAmountWithTax = this.calculateAmountWithTax(sellerItemRequest1.getAmount(),
+                    item.getCgst(), item.getSgst(), item.getIgst(), item.getVat(), item.getCess());
+
+            mapping.setTotalCost(totalAmountWithTax);  // setting the final amount
+            sellerItemMappingRepository.save(mapping);  // saving the seller item mapping
+
+            SellerItemResponse response = this.generateSellerItemResponse(currentSeller, mapping);
+            responses.add(response);
         }
 
+        return responses;
+    }
+
+    /**
+     * method - return the merchant / seller data model by id
+     *
+     * @param id the id of the merchant / seller to be found
+     * @return the associated merchant details with the id
+     */
+    public Seller getSellerById(Long id){
+        return sellerRepository.findById(id).
+                orElseThrow(() -> new InvalidSellerException("Invalid merchant id. Merchant does not exist"));  // returning the seller details
+    }
+
+    /**
+     * Calculates the total amount including applicable taxes.
+     *
+     * @param amount the amount before taxes
+     * @param cgst the CGST percentage
+     * @param sgst the SGST percentage
+     * @param igst the IGST percentage
+     * @param vat the VAT percentage
+     * @param cess the cess percentage
+     * @return the amount after applying all provided taxes
+     */
+    public Double calculateAmountWithTax(Double amount, Float cgst, Float sgst,
+                                         Float igst, Float vat, Float cess) {
+
+        // getting the amounts for the individual tax break down components
+        Double cgstAmount = amount * (cgst) / 100.0;
+        Double sgstAmount = amount * (sgst) / 100.0;
+        Double igstAmount = amount * (igst) / 100.0;
+        Double vatAmount  = amount * (vat) / 100.0;
+        Double cessAmount = amount * (cess) / 100.0;
+
+        return amount + cgstAmount + sgstAmount + igstAmount + vatAmount + cessAmount;  // returning the combined amount
+    }
+
+    /**
+     * Generates item response from the current item
+     *
+     * @param item the item entity
+     * @return the corresponding item response
+     */
+    public ItemResponse generateResponse(Item item){
+        ItemResponse response = new ItemResponse();
+        response.setName(item.getItemName());  // setting the item name
+        response.setCategory(item.getCategory());  // setting the category
+        response.setDescription(item.getItemDescription());  // setting the description
+        response.setSku(item.getSku());  // setting the sku
+
+        // setting the auditing information in the response
+        response.setCreatedOn(item.getCreatedOn());
+        response.setModifiedOn(item.getModifiedOn());
+
+        // setting the taxes information in the response
+        response.setCgst(item.getCgst());
+        response.setSgst(item.getSgst());
+        response.setIgst(item.getIgst());
+        response.setVat(item.getVat());
+        response.setCess(item.getCess());
+        return response;  // returning the item response
+    }
+
+    /**
+     * Generates a seller item response by combining the seller details and the corresponding seller-item mapping information.
+     *
+     * @param seller the seller entity
+     * @param mapping the seller-item mapping entity
+     * @return the generated seller item response
+     */
+    public SellerItemResponse generateSellerItemResponse(Seller seller, SellerItemMapping mapping){
+        SellerItemResponse response = new SellerItemResponse();
+        response.setAmount(mapping.getAmount());
+        response.setName(seller.getSellerName());
+        response.setOtherCharges(mapping.getOtherCharges());
+        response.setTransportationCharges(mapping.getTransportationCharges());
+        response.setFinalAmount(mapping.getTotalCost());
+        response.setQuantity(mapping.getAvailableQuantity());
+        return response;   // returning the response
     }
 }
