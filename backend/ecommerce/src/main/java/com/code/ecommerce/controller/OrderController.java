900bdfbf869da90325a6a660eeaf3e5099c480fe
Niranjan Dhokarikar <niranjan.dhokarikar2001@gmail.com>
2026-06-29 09:48:18 +0530

    Corrected indentation

--- backend/ecommerce/src/main/java/com/code/ecommerce/controller/OrderController.java
+++ backend/ecommerce/src/main/java/com/code/ecommerce/controller/OrderController.java
@@ -2,7 +2,7 @@ package com.code.ecommerce.controller;
 
 import com.code.ecommerce.common.constants.RequestTracker;
 import com.code.ecommerce.dto.requests.orders.OrderRequest;
-import com.code.ecommerce.service.OrderService;
+import com.code.ecommerce.service.orders.OrderService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
