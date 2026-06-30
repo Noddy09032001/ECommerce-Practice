8aa6f181ae71ab0d1eceaea5acb2a7bda9b98228
Niranjan Dhokarikar <niranjan.dhokarikar2001@gmail.com>
2026-06-30 10:45:42 +0530

    Methods for fetching merchant item mapping in repository layer

--- backend/ecommerce/src/main/java/com/code/ecommerce/repository/SellerItemMappingRepository.java
+++ backend/ecommerce/src/main/java/com/code/ecommerce/repository/SellerItemMappingRepository.java
@@ -2,8 +2,16 @@ package com.code.ecommerce.repository;
 
 import com.code.ecommerce.pojo.SellerItemMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface SellerItemMappingRepository extends JpaRepository<SellerItemMapping, Long> {
+
+    @Query("SELECT sim FROM SellerItemMapping sim WHERE sim.item.id = :itemId AND sim.seller.id = :sellerId")
+    SellerItemMapping findByItemAndSeller(@Param("itemId") Long itemId,
+            @Param("sellerId") Long sellerId); // getting the item by the item id and the seller id
 }
