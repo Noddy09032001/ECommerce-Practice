3978df05d411719390d902702c8806ff970a9855
Niranjan Dhokarikar <niranjan.dhokarikar2001@gmail.com>
2026-07-01 08:48:31 +0530

    Order constants added

--- backend/ecommerce/src/main/java/com/code/ecommerce/common/constants/OrderStatus.java
+++ backend/ecommerce/src/main/java/com/code/ecommerce/common/constants/OrderStatus.java
@@ -2,9 +2,19 @@ package com.code.ecommerce.common.constants;
 
 // tracking the statuses of the orders generated
 public enum OrderStatus {
-    ORDER_CREATED,  // order has been created
-    ORDER_PLACED,  // order has been placed
-    ORDER_DISPATCHED,  // order has been dispatched
-    ORDER_RECEIVED,  // order has been received by the user
-    ORDER_CANCELLED,  // order has been canceled by the user
+    ORDER_CREATED("Order has been created successfully"),  // order has been created
+    ORDER_PLACED("Order has been placed successfully"),  // order has been placed
+    ORDER_DISPATCHED("Order has been dispatched successfully"),  // order has been dispatched
+    ORDER_RECEIVED("Order has been received by the user"),  // order has been received by the user
+    ORDER_CANCELLED("Order has been cancelled by the user");  // order has been canceled by the user
+
+    private final String message;   // getting the message from the constant
+
+    OrderStatus(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
