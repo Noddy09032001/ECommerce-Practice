7b4f6613090819257654218761f2555d5f8e7922
Niranjan Dhokarikar <niranjan.dhokarikar2001@gmail.com>
2026-07-05 14:42:23 +0530

    Method added in user repository layer for fetching users via email and phone number

--- backend/ecommerce/src/main/java/com/code/ecommerce/repository/UserDetailsRepository.java
+++ backend/ecommerce/src/main/java/com/code/ecommerce/repository/UserDetailsRepository.java
@@ -2,6 +2,8 @@ package com.code.ecommerce.repository;
 
 import com.code.ecommerce.pojo.UserDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,4 +11,7 @@ public interface UserDetailsRepository extends JpaRepository<UserDetails, Long> 
     UserDetails findUserDetailsByEmail(String email);  // getting the user details by email
     // method to fetch the user by the username and password
     // method to fetch the user by the email and password
+
+    @Query("SELECT u FROM UserDetails u WHERE u.email=:email AND u.phoneNumber=:phone")
+    UserDetails findUserDetailsByEmailAndPhoneNumber(@Param("email") String email, @Param("phone") String phoneNumber);
 }
