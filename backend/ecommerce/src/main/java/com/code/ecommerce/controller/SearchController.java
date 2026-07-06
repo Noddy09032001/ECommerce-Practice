f39d22403e87990637882358da76d3bd7a4d0d99
Niranjan Dhokarikar <niranjan.dhokarikar2001@gmail.com>
2026-07-06 13:52:51 +0530

    Methods for order search

--- backend/ecommerce/src/main/java/com/code/ecommerce/controller/SearchController.java
+++ backend/ecommerce/src/main/java/com/code/ecommerce/controller/SearchController.java
@@ -1,6 +1,7 @@
 package com.code.ecommerce.controller;
 
 import com.code.ecommerce.dto.requests.ItemSearchRequest;
+import com.code.ecommerce.dto.requests.orders.OrderSearchRequest;
 import com.code.ecommerce.dto.response.ApiResponse;
 import com.code.ecommerce.dto.response.ItemCatalogueResponse;
 import com.code.ecommerce.service.search.SearchService;
@@ -36,6 +37,22 @@ public class SearchController {
         ApiResponse apiResponse = new ApiResponse();
         apiResponse.setData(response);  // setting the response in the data of the api response body
         apiResponse.setMessage("Data for the search filters for the item found successfully");  // setting the message
+        apiResponse.setStatusCode(HttpStatus.OK);  // setting the status code
+        return ResponseEntity.ok(apiResponse);  // setting and returning the response entity
+    }
+
+    /**
+     * Searches for the orders for the given user
+     *
+     * @param request search request containing the search parameters for the order
+     * @return paginated catalogue response for the given search parameters
+     */
+    @PostMapping("/orders-search")
+    public ResponseEntity<?> searchOrders(@RequestBody OrderSearchRequest request){
+        Page<?> response = searchService.searchOrder(request);  // getting the paginated response
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setData(response);  // setting the response in the data of the api response body
+        apiResponse.setMessage("Data for the search filters for the orders found successfully");  // setting the message
         apiResponse.setStatusCode(HttpStatus.OK);  // setting the status code
         return ResponseEntity.ok(apiResponse);  // setting and returning the response entity
     }
