48b7feb0e2b3fb1c43d47776c5c833274b482c7e
Niranjan Dhokarikar <niranjan.dhokarikar2001@gmail.com>
2026-07-13 19:40:19 +0530

    Changes to the product info component for including search params

--- frontend/ecommerce/src/modules/products/components/ProductsInfo.tsx
+++ frontend/ecommerce/src/modules/products/components/ProductsInfo.tsx
@@ -3,100 +3,75 @@
 import FilterSidebar from "@/src/common/components/catalogue/FilterSidebar";
 import ProductGrid from "@/src/common/components/catalogue/ProductGrid";
 import ProductPreview from "@/src/common/components/catalogue/ProductPreview";
-import PageHeading from "@/src/common/components/elements/PageHeading";
 import { Product } from "@/src/common/types/products";
-import { useState } from "react";
-
-const products: Product[] = [
-  {
-    itemId: "ITM-1",
-    itemName: "iPhone 16 Pro Max",
-    sku: "IPH001",
-    itemDescription: "Apple iPhone 16 Pro Max 256GB Titanium Edition",
-    images: [
-      {
-        id: "1",
-        url: "https://images.unsplash.com/photo-1592750475338-74b7b21085ab",
-      },
-      {
-        id: "2",
-        url: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9",
-      },
-      {
-        id: "3",
-        url: "https://images.unsplash.com/photo-1580910051074-3eb694886505",
-      },
-    ],
-
-    sellers: [
-      {
-        seller: {
-          sellerId: "SELLER-1",
-          sellerName: "Vijay Sales",
-        },
-        amount: 100000,
-        transportationCharges: 500,
-        otherCharges: 100,
-        totalCost: 118600,
-        availableQuantity: 10,
-      },
-
-      {
-        seller: {
-          sellerId: "SELLER-2",
-          sellerName: "iVenus",
-        },
-        amount: 99000,
-        transportationCharges: 200,
-        otherCharges: 50,
-        totalCost: 116820,
-        availableQuantity: 5,
-      },
-    ],
-  },
-
-  {
-    itemId: "ITM-2",
-    itemName: "AirPods Pro",
-    sku: "AIR001",
-    itemDescription: "Apple AirPods Pro 2nd Generation",
-    images: [
-      {
-        id: "1",
-        url: "https://images.unsplash.com/photo-1606220945770-b5b6c2c55bf1",
-      },
-      {
-        id: "2",
-        url: "https://images.unsplash.com/photo-1572569511254-d8f925fe2cbb",
-      },
-      {
-        id: "3",
-        url: "https://images.unsplash.com/photo-1545127398-14699f92334b",
-      },
-    ],
-
-    sellers: [
-      {
-        seller: {
-          sellerId: "SELLER-1",
-          sellerName: "Vijay Sales",
-        },
-        amount: 22000,
-        transportationCharges: 100,
-        otherCharges: 50,
-        totalCost: 26000,
-        availableQuantity: 25,
-      },
-    ],
-  },
-];
-
-const PAGE_TITLE = 'Our Product Catalogue';
-const PAGE_DESCRIPTION = "Browse our extensive range of high-quality products designed to meet your needs. Discover the latest arrivals, best-selling items, and exclusive collections—all in one place.";
+import { useEffect, useState } from "react";
+import { fetchAllItems } from "@/src/common/api/itemsApiService";
+import { ItemSearchRequest } from "@/src/common/types/search";
+import { searchItems } from "@/src/common/api/searchService";
 
 
 const ProductInfo = () => {
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+
+  // fetching all the items from the api
+  const [products, setProducts] = useState<Product[]>([]); // storing the items in the product type format
+
+  const [loading, setLoading] = useState(false);
+  const [totalPages, setTotalPages] = useState(0); // setting the total pages for the pagination
+  const [totalElements, setTotalElements] = useState(0); // setting the total elements per page for pagination
+
+  const [filters, setFilters] = useState<ItemSearchRequest>({
+    categories: [],
+    sellerIds: [],
+
+    minPrice: undefined,
+    maxPrice: undefined,
+
+    searchText: "",
+
+    sortBy: "NAME",
+
+    page: 0,
+    size: 12,
+  });
+
+  /**
+   * Fetch products whenever filters change.
+   */
+  useEffect(() => {
+    loadProducts();
+  }, [filters]);
+
+  /**
+   * Loads the catalogue from the backend.
+   */
+  const loadProducts = async () => {
+    try {
+      setLoading(true);
+      const page = await searchItems(filters);
+
+      setProducts(page.content);  // setting the products informations
+      setTotalPages(page.totalPages);  // setting the total number of pages
+      setTotalElements(page.totalElements);  // setting the total number of elements returned
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  /*useEffect(() => {
+    const loadProducts = async () => {
+        try {
+            const response = await fetchAllItems();  // calling the api to get the details of the items
+            console.log("Items: ",response); // getting the items response on the console
+            setItems(response.data);  // setting the response in the items 
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    loadProducts();
+  }, []); */
 
   return (
     <main className="min-h-screen bg-background text-foreground transition-colors">
@@ -110,14 +85,30 @@ const ProductInfo = () => {
         >
           {/* filters */}
 
-          <FilterSidebar />
+          <FilterSidebar filters={filters}
+            onChange={(updatedFilters) => {
+              setFilters({
+                ...updatedFilters,
+                page: 0,
+              });
+            }}/>
 
           {/* catalogue */}
 
           <ProductGrid
             products={products}
+            loading={loading}
+            totalElements={totalElements}
+            currentPage={filters.page}
+            totalPages={totalPages}
             onSelect={setSelectedProduct}
             previewOpen={selectedProduct !== null}
+            onPageChange={(page) =>
+              setFilters({
+                ...filters,
+                page,
+              })
+            }
           />
 
           {/* preview */}
