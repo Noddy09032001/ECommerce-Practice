24d24c96a6e538d321610be740d3a5da4bdaa73b
Niranjan Dhokarikar <niranjan.dhokarikar2001@gmail.com>
2026-07-14 10:18:44 +0530

    Modified main routing path for search methods

--- backend/ecommerce/src/main/java/com/code/ecommerce/controller/SearchController.java
+++ backend/ecommerce/src/main/java/com/code/ecommerce/controller/SearchController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/search")
+@RequestMapping("/api/v1/search")
 public class SearchController {
 
     private final SearchService searchService;
