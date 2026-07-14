7f1487c1d6b0c68d1336d6a8f819cd5420541d38
Niranjan Dhokarikar <niranjan.dhokarikar2001@gmail.com>
2026-07-14 10:16:56 +0530

    Modified generic configurations for security

--- backend/ecommerce/src/main/java/com/code/ecommerce/common/configs/GenericConfigurations.java
+++ backend/ecommerce/src/main/java/com/code/ecommerce/common/configs/GenericConfigurations.java
@@ -2,6 +2,10 @@ package com.code.ecommerce.common.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -9,6 +13,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 @Configuration
+@EnableMethodSecurity
 public class GenericConfigurations {
 
     /**
@@ -18,7 +23,7 @@ public class GenericConfigurations {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://192.168.1.7:3000", "http://192.168.1.8:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", "http://192.168.1.7:3000", "http://192.168.1.8:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
@@ -27,5 +32,24 @@ public class GenericConfigurations {
         source.registerCorsConfiguration("/**", configuration);
 
         return source;
+    }
+
+    /**
+     * Configures the application's Spring Security filter chain.
+     * @param http the {@link HttpSecurity} instance used to configure web security
+     * @return the configured {@link SecurityFilterChain}
+     * @throws Exception if an error occurs while configuring the security filter chain
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf -> csrf.disable())
+                .cors(cors -> {})
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable())
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().permitAll()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
     }
 }
