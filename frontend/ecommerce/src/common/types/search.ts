f52ba954da5f7fd3d63e8a8201246eb8b4b95823
Niranjan Dhokarikar <niranjan.dhokarikar2001@gmail.com>
2026-07-20 11:36:45 +0530

    Added type definitions for order search

--- frontend/ecommerce/src/common/types/search.ts
+++ frontend/ecommerce/src/common/types/search.ts
@@ -12,3 +12,44 @@ export interface ItemSearchRequest {
     page: number;  // the current page number for pagination
     size: number;  // the number of items per page 
 }
+
+export interface OrderSearchRequest {
+  userName?: string;   // the username of the user
+  email?: string;  // the email of the user
+
+  searchText?: string;  // the search text for the order containing the order id
+
+  minOrderAmount?: number;  // the min order amount
+  maxOrderAmount?: number;  // the max order amount
+
+  fromDate?: string;  // from date filter for order
+  toDate?: string;   // to date filter for the order
+
+  currentStatus?: string;  // current status of the order
+
+  sortBy?: string;  // filters for order sorting
+
+  page: number;   // the current page number for pagination
+  size: number;  // the number of items per page 
+}
+
+export interface PageResponse<T> {
+  content: T[];
+  totalPages: number;
+  totalElements: number;
+  size: number;
+  number: number;
+}
+
+export interface OrderSearchResponse {
+    orderId: string;
+    currentStatus: string;
+    grandTotal: number;
+    createdOn: string;
+    orderDetails: {
+        paymentMode: string;
+        address: string;
+        city: string;
+        state: string;
+    };
+}
